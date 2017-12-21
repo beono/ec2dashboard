f497f13e41c78d9bdbfd994876606316879550a8
Eldar Rakhimberdin <ibeono@gmail.com>
2017-12-21 20:33:24 +0100

    Updated template

--- config.dist.js
+++ config.dist.js
@@ -1,6 +1,8 @@
 var ec2dashboardConfig = {
     "hcpath": "health",
     "accessKeyId": "***",
+    // For security reasons you can also remove "secretAccessKey" line and set the value when you open the application in browser
+    "secretAccessKey": "***",
     "region": "eu-east-1",
     "services": "users_api,news_api,search_api"
 };
